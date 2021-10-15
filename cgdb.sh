29c60a47933fe12e68cf4d2da738103772140c42
Marco Figueiredo <marcoacfbr@yahoo.com.br>
2021-10-15 13:56:26 -0300

    Delete cgdb.sh

--- cgdb.sh
+++
@@ -1,6 +0,0 @@
-## autoreconf --force --install
-## ./configure > nul
-make clean
-make | grep erro 
-##./masa-opencl --no-flush --stage-1 --no-block-pruning AE002160.2.fasta CP000051.1.fasta
-gdb masa-opencl
