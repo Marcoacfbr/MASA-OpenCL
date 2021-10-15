7ad94311da1768aa8470ee8cba6fba6d32c2109e
Marco Figueiredo <marcoacfbr@yahoo.com.br>
2021-10-15 13:56:45 -0300

    Delete comp.sh

--- comp.sh
+++
@@ -1,8 +0,0 @@
-## autoreconf --force --install
-rm -rf work.tmp
-#./configure > nul
-make clean
-make | grep erro 
-# ./masa-opencl --no-flush --stage-1 --no-block-pruning AE002160.2.fasta CP000051.1.fasta
-#gdb masa-opencl
-#./masa-opencl --no-flush --stage-1 BA000035.2.fasta BX927147.1.fasta
