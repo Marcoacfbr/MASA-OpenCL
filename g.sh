ba22c006efc1b734619643a4e90dad15a7c33d23
Marco Figueiredo <marcoacfbr@yahoo.com.br>
2021-10-15 09:52:13 -0700

    %1

--- g.sh
+++ g.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add -A .
 git commit -m %1
-git push https://Marcoacfbr@github.com/Marcoacfbr/MASA-OpenCL.git
+git push https://Marcoacfbr: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@github.com/Marcoacfbr/MASA-OpenCL.git
 
